d4ae441f08b197080d7131bd166684d1deb22c72
Katsuki <Katsuki_Sato0130@icloud.com>
2019-06-22 10:37:34 +0900

    Add createBook

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var book Book
-	_ = json.NewDecoder(r.body).Decode(&book)
+	_ = json.NewDecoder(r.Body).Decode(&book)
 	book.ID = strconv.Itoa(rand.Intn(1000000)) // Mock ID - not safe in production
 	books = append(books, book)
 	json.NewEncoder(w).Encode(book)
